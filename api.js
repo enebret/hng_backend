9cfa8285bd3ecb41f7c1b0523643462ccfdcabd0
Ikenna Ene <enebret@yahoo.com>
2022-10-30 19:00:41 +0100

    Update api.js

--- api.js
+++ api.js
@@ -4,12 +4,17 @@ const app = express();
 var PORT = 3520;
 
 app.get('/', (req, res) => {
-res.writeHead(200, { "Content-Type": "application/json" });
-res.json({ slackUsername: 'awsumbret',
+res.statusCode = 200; 
+res.type('json')
+var rs = {
+    slackUsername: 'awsumbret',
 backend: true,
 age: 28,
 bio: "I am a FullStack Freelance developer that employs the use of Javascript and its frameworks to create web experiences"
-})});
+}
+const jsonContent = JSON.stringify(rs);
+res.end(jsonContent);
+});
 app.listen(PORT, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", PORT);
