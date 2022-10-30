e8887c8505066f6dab8ef3003a8f6dc4683e09d6
ene ikenna <enebret@yahoo.com>
2022-10-30 18:06:03 +0100

    first commit

---
+++ api.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const hostname = '127.0.0.1';
+const app = express();
+var PORT = 3520;
+
+app.get('/', (req, res) => {
+res.statusCode = 200; 
+res.json({ slackUsername: 'awsumbret',
+backend: true,
+age: 28,
+bio: "I am a FullStack Freelance developer that employs the use of Javascript and its frameworks to create web experiences"
+})});
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+    console.log(`HTTP Server listening at http://${hostname}:${PORT}/`)
+});
